cd2a2a1a17233ea1e269b4c05e531c9528ad981a
VadymIakymchuk <126196895+VadymIakymchuk@users.noreply.github.com>
2023-02-23 20:06:08 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 
+
+
 void setup() {
   Serial.begin(115200);
-  Serial.print("Hola mundo");
+  Serial.print("afesdzxgds");
   pinMode(23,OUTPUT);
+  
   // put your setup code here, to run once:
 }
 
 void loop() {
+  digitalWrite(23,HIGH);
   Serial.println("ON");
+  Serial.println(temperatureRead());
+  Serial.println(analogRead(15));
   delay(200);
+
+  digitalWrite(23,LOW);
   Serial.println("OFF");
   delay(200);
+
   // put your main code here, to run repeatedly:
 }
